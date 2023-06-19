21ce5375e54cb80d09fe247082999ba590861938
Serge Maia <50481178+s2erge@users.noreply.github.com>
2023-06-19 09:06:33 +0100

    ADD: Carousel Slider JS for Testimonial Section

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+
+/* :: 1.0 Testimonial Slider JS */
+	function testimonialSlider() {
+		const carouselOne = document.getElementById('carouselOne');
+
+		if(carouselOne) {
+			carouselOne.addEventListener('slid.bs.carousel', function () {
+				const activeItem = this.querySelector(".active");
+				document.querySelector(".js-testimonial-img").src = activeItem.getAttribute("data-js-testimonial-img")
+			})
+		}
+
+	} // testimonialSlider ()
+
+	testimonialSlider()
