7ae535d9ddde5bea0896dc9c480b356d3b573545
Serge Maia <50481178+s2erge@users.noreply.github.com>
2023-11-16 07:15:30 +0000

    ADD: Modal Video | Course Details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 
-
 /* :: 1.0 Testimonial Slider JS */
 	function testimonialSlider() {
 		const carouselOne = document.getElementById('carouselOne');
@@ -14,3 +13,20 @@
 	} // testimonialSlider ()
 
 	testimonialSlider()
+
+/* :: 1.0 Course Preview Video JS */
+	function coursePreviewVideo () {
+		const coursePreviewModal = document.querySelector(".js-course-preview-modal")
+		if(coursePreviewModal) {
+			coursePreviewModal.addEventListener("shown.bs.modal", function() {
+				this.querySelector(".js-course-preview-video").play();
+				this.querySelector(".js-course-preview-video").currentTime = 0;
+			});
+
+			coursePreviewModal.addEventListener("hide.bs.modal", function() {
+				this.querySelector(".js-course-preview-video").pause();
+			});
+		}
+	} // coursePreviewVideo ()
+
+	coursePreviewVideo()
